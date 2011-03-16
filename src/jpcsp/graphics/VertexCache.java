EmulatorArchive/jpcsp

commit 85cba28772ae716a160bd08d03f0d6beff1676b8

Small fixes in VideoEngine: - scissor command - offset_addr command: could have been the cause for errors reporting invalid VideoEngine commands - save only the GE to memory when something has been rendered
git-svn-id: http://jpcsp.googlecode.com/svn/trunk@2018 4ab978e6-3352-0410-996c-0b42db0051c8

src/jpcsp/graphics/VideoEngine.java
@@ -186,6 +186,7 @@ public class VideoEngine {
     float[][] bboxVertices;
     private ConcurrentLinkedQueue<PspGeList> drawListQueue;
     private boolean somethingDisplayed;
+    private boolean forceLoadGEToScreen;
     private boolean geBufChanged;
     private IAction hleAction;
     private int[] currentCMDValues;
@@ -494,7 +495,7 @@ public class VideoEngine {
 
         endUpdate();
 
-        return true;
+        return somethingDisplayed;
     }
 
     private void logLevelUpdated() {
@@ -538,6 +539,8 @@ public class VideoEngine {
         logLevelUpdated();
         memoryForGEUpdated();
         somethingDisplayed = false;
+        geBufChanged = true;
+        forceLoadGEToScreen = true;
         textureChanged = true;
         projectionMatrixUpload.setChanged(true);
         modelMatrixUpload.setChanged(true);
@@ -663,6 +666,9 @@ public class VideoEngine {
             // compiling a huge CodeBlock on the first call).
             // This avoids aborting the first list enqueued.
             int maxStallCount = (currentList.pc != currentList.list_addr ? 100 : 400);
+            if (isLogDebugEnabled) {
+            	maxStallCount = Integer.MAX_VALUE;
+            }
 
             if (waitForSyncCount > maxStallCount) {
                 error(String.format("Waiting too long on stall address 0x%08X, aborting the list %s", currentList.pc, currentList));
@@ -934,6 +940,13 @@ public class VideoEngine {
         if (!clutIsDirty || context.tex_clut_addr == 0) {
             return;
         }
+
+		if (!Memory.isAddressGood(context.tex_clut_addr)) {
+			if (isLogWarnEnabled) {
+				log.warn(String.format("Invalid clut address 0x%08X", context.tex_clut_addr));
+			}
+			return;
+		}
 
         if (context.tex_clut_mode == CMODE_FORMAT_32BIT_ABGR8888) {
             readClut32(0);
@@ -2323,6 +2336,7 @@ public class VideoEngine {
         }
 
         updateGeBuf();
+        somethingDisplayed = true;
         loadTexture();
 
         drawBezier(ucount, vcount);
@@ -2342,6 +2356,7 @@ public class VideoEngine {
         }
 
         updateGeBuf();
+        somethingDisplayed = true;
         loadTexture();
 
         drawSpline(sp_ucount, sp_vcount, sp_utype, sp_vtype);
@@ -3534,6 +3549,11 @@ public class VideoEngine {
     private void executeCommandSCISSOR1() {
     	context.scissor_x1 = normalArgument & 0x3ff;
     	context.scissor_y1 = (normalArgument >> 10) & 0x3ff;
+
+    	// Already update width&height in case SCISSOR2 is not coming...
+    	context.scissor_width = 1 + context.scissor_x2 - context.scissor_x1;
+    	context.scissor_height = 1 + context.scissor_y2 - context.scissor_y1;
+
     	scissorChanged = true;
     }
 
@@ -4270,11 +4290,18 @@ public class VideoEngine {
             return;
         }
 
+        int tex_addr = context.texture_base_pointer[0] & Memory.addressMask;
+        if (!Memory.isAddressGood(tex_addr)) {
+        	if (isLogWarnEnabled) {
+        		log.warn(String.format("Invalid texture address 0x%08X for texture level 0", tex_addr));
+        	}
+    		return;
+        }
+
         re.setTextureFormat(context.texture_storage, context.texture_swizzle);
 
         Texture texture;
-        int tex_addr = context.texture_base_pointer[0] & Memory.addressMask;
-        if (!canCacheTexture(tex_addr)) {
+		if (!canCacheTexture(tex_addr)) {
         	if (loadGETexture(tex_addr)) {
                 re.setTextureMipmapMagFilter(context.tex_mag_filter);
                 re.setTextureMipmapMinFilter(context.tex_min_filter);
@@ -5595,7 +5622,7 @@ public class VideoEngine {
 
     private void updateGeBuf() {
         if (geBufChanged) {
-            display.hleDisplaySetGeBuf(context.fbp, context.fbw, context.psm, somethingDisplayed);
+            display.hleDisplaySetGeBuf(context.fbp, context.fbw, context.psm, somethingDisplayed, forceLoadGEToScreen);
             geBufChanged = false;
 
             textureChanged = true;

src/jpcsp/HLE/modules150/sceDisplay.java
@@ -508,11 +508,11 @@ public class sceDisplay extends AWTGLCanvas implements HLEModule, HLEStartModule
         }
     }
 
-    public void hleDisplaySetGeBuf(int topaddr, int bufferwidth, int pixelformat, boolean copyGEToMemory) {
-    	hleDisplaySetGeBuf(topaddr, bufferwidth, pixelformat, copyGEToMemory, widthGe, heightGe);
+    public void hleDisplaySetGeBuf(int topaddr, int bufferwidth, int pixelformat, boolean copyGEToMemory, boolean forceLoadGEToScreen) {
+    	hleDisplaySetGeBuf(topaddr, bufferwidth, pixelformat, copyGEToMemory, forceLoadGEToScreen, widthGe, heightGe);
     }
 
-    public void hleDisplaySetGeBuf(int topaddr, int bufferwidth, int pixelformat, boolean copyGEToMemory, int width, int height) {
+    public void hleDisplaySetGeBuf(int topaddr, int bufferwidth, int pixelformat, boolean copyGEToMemory, boolean forceLoadGEToScreen, int width, int height) {
         topaddr &= Memory.addressMask;
         // We can get the address relative to 0 or already relative to START_VRAM
         if (topaddr < MemoryMap.START_VRAM) {
@@ -526,7 +526,12 @@ public class sceDisplay extends AWTGLCanvas implements HLEModule, HLEStartModule
         if (topaddr == topaddrGe && bufferwidth == bufferwidthGe &&
             pixelformat == pixelformatGe &&
             width == widthGe && height == heightGe) {
+
         	// Nothing changed
+        	if (forceLoadGEToScreen) {
+        		loadGEToScreen();
+        	}
+
         	return;
         }
 
@@ -624,37 +629,7 @@ public class sceDisplay extends AWTGLCanvas implements HLEModule, HLEStartModule
 		checkTemp();
 
 		if (loadGEToScreen) {
-			if (VideoEngine.log.isDebugEnabled()) {
-				VideoEngine.log.debug(String.format("Reloading GE Memory (0x%08X-0x%08X) to screen (%dx%d)", topaddrGe, bottomaddrGe, widthGe, heightGe));
-			}
-
-			if (statisticsCopyMemoryToGe != null) {
-				statisticsCopyMemoryToGe.start();
-			}
-
-        	if (saveGEToTexture && !VideoEngine.getInstance().isVideoTexture(topaddrGe)) {
-				GETexture geTexture = GETextureManager.getInstance().getGETexture(re, topaddrGe, bufferwidthGe, widthGe, heightGe, pixelformatGe);
-				geTexture.copyTextureToScreen(re);
-        	} else {
-            	if (re.isVertexArrayAvailable()) {
-            		re.bindVertexArray(0);
-            	}
-
-				// Set texFb as the current texture
-				re.bindTexture(texFb);
-
-				// Define the texture from the GE Memory
-			    re.setPixelStore(bufferwidthGe, getPixelFormatBytes(pixelformatGe));
-			    int textureSize = bufferwidthGe * heightGe * getPixelFormatBytes(pixelformatGe);
-				re.setTexSubImage(0, 0, 0, bufferwidthGe, heightGe, pixelformatGe, pixelformatGe, textureSize, pixelsGe);
-
-				// Draw the GE
-			    drawFrameBuffer(false, true, bufferwidthGe, pixelformatGe, widthGe, heightGe);
-        	}
-
-			if (statisticsCopyMemoryToGe != null) {
-				statisticsCopyMemoryToGe.end();
-			}
+			loadGEToScreen();
 
 			if (State.captureGeNextFrame) {
 		    	captureGeImage();
@@ -918,6 +893,41 @@ public class sceDisplay extends AWTGLCanvas implements HLEModule, HLEStartModule
 
             Emulator.setFpsTitle(String.format("FPS: %d, averageFPS: %.1f", lastFPS, averageFPS));
         }
+    }
+
+    private void loadGEToScreen() {
+		if (VideoEngine.log.isDebugEnabled()) {
+			VideoEngine.log.debug(String.format("Reloading GE Memory (0x%08X-0x%08X) to screen (%dx%d)", topaddrGe, bottomaddrGe, widthGe, heightGe));
+		}
+
+		if (statisticsCopyMemoryToGe != null) {
+			statisticsCopyMemoryToGe.start();
+		}
+
+    	if (saveGEToTexture && !VideoEngine.getInstance().isVideoTexture(topaddrGe)) {
+			GETexture geTexture = GETextureManager.getInstance().getGETexture(re, topaddrGe, bufferwidthGe, widthGe, heightGe, pixelformatGe);
+			geTexture.copyTextureToScreen(re);
+    	} else {
+        	if (re.isVertexArrayAvailable()) {
+        		re.bindVertexArray(0);
+        	}
+
+			// Set texFb as the current texture
+			re.bindTexture(texFb);
+
+			// Define the texture from the GE Memory
+		    re.setPixelStore(bufferwidthGe, getPixelFormatBytes(pixelformatGe));
+		    int textureSize = bufferwidthGe * heightGe * getPixelFormatBytes(pixelformatGe);
+		    pixelsGe.clear();
+			re.setTexSubImage(0, 0, 0, bufferwidthGe, heightGe, pixelformatGe, pixelformatGe, textureSize, pixelsGe);
+
+			// Draw the GE
+		    drawFrameBuffer(false, true, bufferwidthGe, pixelformatGe, widthGe, heightGe);
+    	}
+
+		if (statisticsCopyMemoryToGe != null) {
+			statisticsCopyMemoryToGe.end();
+		}
     }
 
     /** @param first : true  = draw as psp size
@@ -1204,35 +1214,9 @@ public class sceDisplay extends AWTGLCanvas implements HLEModule, HLEStartModule
         	}
         	re.startDisplay();
 
-        	// Render GE
-            re.setViewport(0, 0, width, height);
-
-            // If the GE is not at the same address as the FrameBuffer,
-            // redisplay the GE so that the VideoEngine can update it
-            if (bottomaddrGe != bottomaddrFb) {
-            	if (log.isDebugEnabled()) {
-            		log.debug(String.format("sceDisplay.paintGL - reloading the GE from memory 0x%08X", topaddrGe));
-            	}
-            	if (saveGEToTexture && !VideoEngine.getInstance().isVideoTexture(topaddrGe)) {
-            		GETexture geTexture = GETextureManager.getInstance().getGETexture(re, topaddrGe, bufferwidthGe, widthGe, heightGe, pixelformatGe);
-            		geTexture.copyTextureToScreen(re);
-            	} else {
-		            pixelsGe.clear();
-		            re.bindTexture(texFb);
-					re.setTextureFormat(pixelformatGe, false);
-		            re.setPixelStore(bufferwidthGe, pixelformatGe);
-		            int textureSize = bufferwidthGe * heightGe * getPixelFormatBytes(pixelformatGe);
-					re.setTexSubImage(0,
-		                0, 0, bufferwidthGe, heightGe,
-		                pixelformatGe,
-		                pixelformatGe,
-		                textureSize, pixelsGe);
-
-					drawFrameBuffer(false, true, bufferwidthFb, pixelformatFb, width, height);
-            	}
-            }
-
+        	// The GE will be reloaded to the screen by the VideoEngine
             if (VideoEngine.getInstance().update()) {
+                // Save the GE only if it actually drew something
             	if (log.isDebugEnabled()) {
             		log.debug(String.format("sceDisplay.paintGL - saving the GE to memory 0x%08X", topaddrGe));
             	}
@@ -1241,7 +1225,6 @@ public class sceDisplay extends AWTGLCanvas implements HLEModule, HLEStartModule
             		GETexture geTexture = GETextureManager.getInstance().getGETexture(re, topaddrGe, bufferwidthGe, widthGe, heightGe, pixelformatGe);
             		geTexture.copyScreenToTexture(re);
             	} else {
-	                // Update VRAM only if GE actually drew something
 	                // Set texFb as the current texture
 	                re.bindTexture(texFb);
 	    			re.setTextureFormat(pixelformatGe, false);
@@ -1257,7 +1240,7 @@ public class sceDisplay extends AWTGLCanvas implements HLEModule, HLEStartModule
             	}
             }
 
-            // Render FB
+            // Render the FB
         	if (log.isDebugEnabled()) {
         		log.debug(String.format("sceDisplay.paintGL - rendering the FB 0x%08X", topaddrFb));
         	}
@@ -1276,9 +1259,10 @@ public class sceDisplay extends AWTGLCanvas implements HLEModule, HLEStartModule
 	                pixelformatFb,
 	                textureSize, pixelsFb);
 
-	            //Call the rotating function (if needed)
-	            if(ang != 4)
+	            // Call the rotating function (if needed)
+	            if (ang != 4) {
 	                rotate(ang);
+	            }
 
 	            drawFrameBuffer(false, true, bufferwidthFb, pixelformatFb, width, height);
         	}

src/jpcsp/graphics/GeCommands.java
@@ -730,7 +730,6 @@ public class GeCommands {
         pureStateCommands[NOP] = true;
         pureStateCommands[BASE] = true;
         pureStateCommands[VTYPE] = true;
-        pureStateCommands[OFFSET_ADDR] = true;
         pureStateCommands[REGION1] = true;
         pureStateCommands[REGION2] = true;
         pureStateCommands[LTE] = true;

src/jpcsp/graphics/capture/CaptureDisplayDetails.java
@@ -164,7 +164,7 @@ public class CaptureDisplayDetails {
 
         // This is almost side effect free, but replay is going to trash the emulator state anyway
         display.hleDisplaySetFrameBuf(topaddrFb, bufferwidthFb, pixelformatFb, sync);
-        display.hleDisplaySetGeBuf(fbp, fbw, psm, false);
+        display.hleDisplaySetGeBuf(fbp, fbw, psm, false, false);
 
         if (captureRenderTargets) {
             drawBuffer.commit();

commit 49a2b01ea4ec7aaa169fe03fc6836c75903d4ddc

Improved the memory management for the Vertex Cache (avoid reallocating direct buffers when not necessary, the garbage collector seems to have problems with them). Fixed a problem introduced by r2018.

src/jpcsp/graphics/VertexCache.java
@@ -68,14 +68,15 @@ public class VertexCache {
 		return cache.containsKey(getKey(vertexInfo));
 	}
 
-	protected VertexInfo getVertex(VertexInfo vertexInfo) {
+	protected synchronized VertexInfo getVertex(VertexInfo vertexInfo) {
 		return cache.get(getKey(vertexInfo));
 	}
 
-	public void addVertex(IRenderingEngine re, VertexInfo vertexInfo, int numberOfVertex, float[][] boneMatrix, int numberOfWeightsForShader) {
+	public synchronized void addVertex(IRenderingEngine re, VertexInfo vertexInfo, int numberOfVertex, float[][] boneMatrix, int numberOfWeightsForShader) {
 		Integer key = getKey(vertexInfo);
 		VertexInfo previousVertex = cache.get(key);
 		if (previousVertex != null) {
+		    vertexInfo.reuseCachedBuffer(previousVertex);
 		    previousVertex.deleteVertex(re);
 		} else {
 			// Check if the cache is not growing too large

src/jpcsp/graphics/VertexInfo.java
@@ -741,7 +741,13 @@ public class VertexInfo {
 			needSetDataPointers = true;
 		}
 
-		cachedBuffer = ByteBuffer.allocateDirect(size * VideoEngine.SIZEOF_FLOAT).order(ByteOrder.LITTLE_ENDIAN);
+		int bufferSize = size * VideoEngine.SIZEOF_FLOAT;
+		if (cachedBuffer == null || cachedBuffer.capacity() < bufferSize) {
+			cachedBuffer = ByteBuffer.allocateDirect(bufferSize).order(ByteOrder.LITTLE_ENDIAN);
+		} else {
+			cachedBuffer.clear();
+		}
+
 		int oldLimit = buffer.limit();
 		buffer.limit(size);
 		cachedBuffer.asFloatBuffer().put(buffer);
@@ -840,6 +846,17 @@ public class VertexInfo {
 		}
 
 		readForCache(numberOfVertex);
+	}
+
+	public void reuseCachedBuffer(VertexInfo vertexInfo) {
+		if (vertexInfo != null && vertexInfo.cachedBuffer != null) {
+			// Reuse the cachedBuffer if we don't have one or if we have a smaller one
+			if (cachedBuffer == null || cachedBuffer.capacity() < vertexInfo.cachedBuffer.capacity()) {
+				// Reuse the cachedBuffer
+				cachedBuffer = vertexInfo.cachedBuffer;
+				vertexInfo.cachedBuffer = null;
+			}
+		}
 	}
 
 	@Override

src/jpcsp/graphics/VideoEngine.java
@@ -5623,6 +5623,7 @@ public class VideoEngine {
     private void updateGeBuf() {
         if (geBufChanged) {
             display.hleDisplaySetGeBuf(context.fbp, context.fbw, context.psm, somethingDisplayed, forceLoadGEToScreen);
+            forceLoadGEToScreen = false;
             geBufChanged = false;
 
             textureChanged = true;