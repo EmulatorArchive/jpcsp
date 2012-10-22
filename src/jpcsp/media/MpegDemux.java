EmulatorArchive/jpcsp

commit 2b624003c6d18fe0f3bbcda80dfdfa53687c897e

Added support for Mpeg videos containing multiple audio streams. Improved sceMpegGetUserdataAu and sceMpegQueryUserdataEsSize.
git-svn-id: http://jpcsp.googlecode.com/svn/trunk@2796 4ab978e6-3352-0410-996c-0b42db0051c8

src/jpcsp/media/MpegDemux.java
@@ -185,15 +185,17 @@ public class MpegDemux {
 		if (demuxStream) {
 			PesHeader pesHeader = new PesHeader(channel);
 			length = readPesHeader(pesHeader, length, startCode);
-			if (unescape) {
-				MpegStream mpegStream = new MpegStream(buffer, index, length);
-				while (!mpegStream.isEmpty()) {
-					int b = mpegStream.read8();
-					byteStream.put((byte) b);
+			if (pesHeader.channel == channel) {
+				if (unescape) {
+					MpegStream mpegStream = new MpegStream(buffer, index, length);
+					while (!mpegStream.isEmpty()) {
+						int b = mpegStream.read8();
+						byteStream.put((byte) b);
+					}
+				} else {
+					ByteBuffer mpegStream = ByteBuffer.wrap(buffer, index, length);
+					byteStream.put(mpegStream);
 				}
-			} else {
-				ByteBuffer mpegStream = ByteBuffer.wrap(buffer, index, length);
-				byteStream.put(mpegStream);
 			}
 			skip(length);
 			if (log.isDebugEnabled()) {
@@ -204,7 +206,7 @@ public class MpegDemux {
 		}
 	}
 
-	public void demux(boolean demuxVideo, boolean demuxAudio) {
+	public void demux(boolean demuxVideo, boolean demuxAudio, int videoChannel, int audioChannel) {
 		if (demuxVideo) {
 			videoStream = ByteBuffer.allocate(length - index);
 		}
@@ -240,7 +242,7 @@ public class MpegDemux {
 				}
 				case PRIVATE_STREAM_1: {
 					// Audio stream
-					demuxStream(demuxAudio, startCode, startCode, audioStream, false);
+					demuxStream(demuxAudio, startCode, audioChannel, audioStream, false);
 					break;
 				}
 				case 0x1E0: case 0x1E1: case 0x1E2: case 0x1E3:
@@ -248,7 +250,9 @@ public class MpegDemux {
 				case 0x1E8: case 0x1E9: case 0x1EA: case 0x1EB:
 				case 0x1EC: case 0x1ED: case 0x1EE: case 0x1EF: {
 					// Video Stream
-					demuxStream(demuxVideo, startCode, startCode & 0x0F, videoStream, true);
+					if ((startCode & 0x0F) == videoChannel) {
+						demuxStream(demuxVideo, startCode, videoChannel, videoStream, true);
+					}
 					break;
 				}
 				default: {

src/jpcsp/HLE/modules150/sceMpeg.java
@@ -106,6 +106,7 @@ public class sceMpeg extends HLEModule {
         atracStreamsMap = new HashMap<Integer, Integer>();
         avcStreamsMap = new HashMap<Integer, Integer>();
         pcmStreamsMap = new HashMap<Integer, Integer>();
+        dataStreamsMap = new HashMap<Integer, Integer>();
         mpegAtracAu = new SceMpegAu();
         mpegAvcAu = new SceMpegAu();
         afterRingbufferPutCallback = new AfterRingbufferPutCallback();
@@ -207,6 +208,9 @@ public class sceMpeg extends HLEModule {
     // MPEG PCM elementary stream.
     protected static final int MPEG_PCM_ES_SIZE = 320;
     protected static final int MPEG_PCM_ES_OUTPUT_SIZE = 320;
+    // MPEG Userdata elementary stream.
+    protected static final int MPEG_DATA_ES_SIZE = 0xA0000;
+    protected static final int MPEG_DATA_ES_OUTPUT_SIZE = 0xA0000;
     // MPEG analysis results.
     public static final int MPEG_VERSION_0012 = 0;
     public static final int MPEG_VERSION_0013 = 1;
@@ -228,9 +232,11 @@ public class sceMpeg extends HLEModule {
     protected HashMap<Integer, Integer> atracStreamsMap;
     protected HashMap<Integer, Integer> avcStreamsMap;
     protected HashMap<Integer, Integer> pcmStreamsMap;
+    protected HashMap<Integer, Integer> dataStreamsMap;
     protected boolean isAtracRegistered = false;
     protected boolean isAvcRegistered = false;
     protected boolean isPcmRegistered = false;
+    protected boolean isDataRegistered = false;
     protected boolean ignoreAtrac = false;
     protected boolean ignoreAvc = false;
     protected boolean ignorePcm = false;
@@ -546,6 +552,15 @@ public class sceMpeg extends HLEModule {
     	}
     }
 
+    public int getRegisteredAudioChannel() {
+    	// Return the first registered ATRAC stream
+    	for (Integer streamNumber : atracStreamsMap.values()) {
+    		return streamNumber;
+    	}
+
+    	return -1;
+    }
+
     public int hleMpegGetAvcAu(TPointer auAddr, int firstTimestamp, int noMoreDataError) {
     	int result = 0;
 
@@ -554,7 +569,7 @@ public class sceMpeg extends HLEModule {
         	Emulator.getClock().pause();
         	me.setFirstTimestamp(firstTimestamp);
             if (me.getContainer() == null) {
-                me.init(meChannel, true, true);
+                me.init(meChannel, true, true, getRegisteredAudioChannel());
             }
         	if (!me.readVideoAu(mpegAvcAu)) {
         		// end of video reached only when last timestamp has been reached
@@ -599,7 +614,7 @@ public class sceMpeg extends HLEModule {
         	Emulator.getClock().pause();
         	me.setFirstTimestamp(firstTimestamp);
         	if (me.getContainer() == null) {
-        		me.init(meChannel, true, true);
+        		me.init(meChannel, true, true, getRegisteredAudioChannel());
         	}
         	if (!me.readAudioAu(mpegAtracAu)) {
         		endOfAudioReached = true;
@@ -972,6 +987,10 @@ public class sceMpeg extends HLEModule {
         setCurrentMpegAnalyzed(false);
         unregisterRingbufferPutIoListener();
         VideoEngine.getInstance().resetVideoTextures();
+        atracStreamsMap.clear();
+        pcmStreamsMap.clear();
+        avcStreamsMap.clear();
+        dataStreamsMap.clear();
     }
 
     /**
@@ -1180,6 +1199,10 @@ public class sceMpeg extends HLEModule {
                 isPcmRegistered = true;
                 pcmStreamsMap.put(uid, stream_num);
                 break;
+            case MPEG_DATA_STREAM:
+            	isDataRegistered = true;
+            	dataStreamsMap.put(uid, stream_num);
+            	break;
             default:
                 log.warn("sceMpegRegistStream unknown stream type=" + stream_type);
                 break;
@@ -1219,6 +1242,10 @@ public class sceMpeg extends HLEModule {
                 isPcmRegistered = false;
                 pcmStreamsMap.remove(streamUid);
                 break;
+            case MPEG_DATA_STREAM:
+            	isDataRegistered = false;
+            	dataStreamsMap.remove(streamUid);
+            	break;
             default:
                 log.warn("sceMpegUnRegistStream unknown stream=0x" + Integer.toHexString(streamUid));
                 break;
@@ -1485,7 +1512,7 @@ public class sceMpeg extends HLEModule {
             	Emulator.getClock().pause();
             	me.setFirstTimestamp(audioFirstTimestamp);
             	if (me.getContainer() == null) {
-            		me.init(meChannel, true, true);
+            		me.init(meChannel, true, true, getRegisteredAudioChannel());
             	}
             	if (!me.readAudioAu(mpegAtracAu)) {
             		result = SceKernelErrors.ERROR_MPEG_NO_DATA; // No more data in ringbuffer.
@@ -2303,14 +2330,21 @@ public class sceMpeg extends HLEModule {
 
     @HLEUnimplemented
     @HLEFunction(nid = 0x01977054, version = 150)
-    public int sceMpegGetUserdataAu() {
-        return 0;
+    public int sceMpegGetUserdataAu(@CheckArgument("checkMpegHandle") int mpeg, int streamUid, TPointer auAddr, @CanBeNull TPointer32 resultAddr) {
+    	// 2 Unknown result values
+    	resultAddr.setValue(0, 0);
+    	resultAddr.setValue(4, 0);
+
+    	return 0;
     }
 
     @HLEUnimplemented
     @HLEFunction(nid = 0xC45C99CC, version = 150)
-    public int sceMpegQueryUserdataEsSize() {
-        return 0;
+    public int sceMpegQueryUserdataEsSize(@CheckArgument("checkMpegHandle") int mpeg, TPointer32 esSizeAddr, TPointer32 outSizeAddr) {
+    	esSizeAddr.setValue(MPEG_DATA_ES_SIZE);
+    	outSizeAddr.setValue(MPEG_DATA_ES_OUTPUT_SIZE);
+
+    	return 0;
     }
 
     @HLEUnimplemented

src/jpcsp/HLE/modules150/scePsmfPlayer.java
@@ -281,7 +281,7 @@ public class scePsmfPlayer extends HLEModule {
 	            me = new MediaEngine();
 	            audioDecodeBuffer = new byte[audioSamplesBytes];
 	            me.init(pmfFileData);
-	            me.init(pmfFileChannel, true, true);
+	            me.init(pmfFileChannel, true, true, Modules.sceMpegModule.getRegisteredAudioChannel());
 	        }
     	}
     }

src/jpcsp/connector/AtracCodec.java
@@ -221,7 +221,7 @@ public class AtracCodec {
         			Modules.log.info("AT3+ data decoded by the external decoder.");
         			me.finish();
         			atracChannel = null;
-        			me.init(new FileProtocolHandler(decodedFile), false, true);
+        			me.init(new FileProtocolHandler(decodedFile), false, true, 0);
                     atracEndSample = -1;
         			return;
         		} else if (requireAllAtracData) {
@@ -348,7 +348,7 @@ public class AtracCodec {
         	        			Modules.log.info("AT3+ data decoded by the external decoder (all AT3+ data retrieved).");
         	        			me.finish();
         	        			atracChannel = null;
-        	        			me.init(new FileProtocolHandler(decodedFile), false, true);
+        	        			me.init(new FileProtocolHandler(decodedFile), false, true, 0);
         	                    atracEndSample = -1;
         	        		} else {
         	        			Modules.log.info("AT3+ data could not be decoded by the external decoder, even after retrieving all AT3+ data.");
@@ -368,7 +368,7 @@ public class AtracCodec {
         				Memory.getInstance().memset(address, (byte) 0, samples * 4); 
         			}
         		} else if (atracChannel.length() >= getAtracChannelStartLength() || atracChannel.length() >= atracFileSize) {
-    				me.init(atracChannel, false, true);
+    				me.init(atracChannel, false, true, 0);
     			} else {
     				// Fake returning 1 sample with remainFrames == 0
     				// to force a call to sceAtracAddStreamData.

src/jpcsp/media/ExternalDecoder.java
@@ -171,7 +171,7 @@ public class ExternalDecoder {
 		return true;
     }
 
-    public boolean decodeExtAudio(byte[] mpegData, int mpegFileSize, int mpegOffset) {
+    public boolean decodeExtAudio(byte[] mpegData, int mpegFileSize, int mpegOffset, int audioChannel) {
     	if (dumpPmfFile) {
 			try {
 				new File(MediaEngine.getExtAudioBasePath(mpegFileSize)).mkdirs();
@@ -185,7 +185,7 @@ public class ExternalDecoder {
 
     	MpegDemux mpegDemux = new MpegDemux(mpegData, mpegOffset);
     	try {
-    		mpegDemux.demux(false, true);
+    		mpegDemux.demux(false, true, -1, audioChannel);
 		} catch (OutOfMemoryError e) {
 			log.error(String.format("Error '%s' while decoding external audio file (mpegFileSize=%d)", e.toString(), mpegFileSize));
 			return false;
@@ -199,7 +199,7 @@ public class ExternalDecoder {
 		if (dumpAudioStreamFile) {
 			try {
 				new File(MediaEngine.getExtAudioBasePath(mpegFileSize)).mkdirs();
-				FileOutputStream pmfOut = new FileOutputStream(MediaEngine.getExtAudioPath(mpegFileSize, "audio"));
+				FileOutputStream pmfOut = new FileOutputStream(MediaEngine.getExtAudioPath(mpegFileSize, audioChannel, "audio"));
 				pmfOut.getChannel().write(audioStream);
 				audioStream.rewind();
 				pmfOut.close();
@@ -215,12 +215,12 @@ public class ExternalDecoder {
 
 		try {
 			new File(MediaEngine.getExtAudioBasePath(mpegFileSize)).mkdirs();
-			String encodedFileName = MediaEngine.getExtAudioPath(mpegFileSize, "oma");
+			String encodedFileName = MediaEngine.getExtAudioPath(mpegFileSize, audioChannel, "oma");
 			FileOutputStream os = new FileOutputStream(encodedFileName);
 			os.getChannel().write(omaBuffer);
 			os.close();
 
-			String decodedFileName = MediaEngine.getExtAudioPath(mpegFileSize, "wav");
+			String decodedFileName = MediaEngine.getExtAudioPath(mpegFileSize, audioChannel, "wav");
 
 			if (!executeExternalDecoder(encodedFileName, decodedFileName, keepOmaFile)) {
 				return false;
@@ -233,7 +233,7 @@ public class ExternalDecoder {
 		return true;
     }
 
-    public void decodeExtAudio(int address, int mpegFileSize, int mpegOffset, byte[] bufferHeaderData) {
+    public void decodeExtAudio(int address, int mpegFileSize, int mpegOffset, byte[] bufferHeaderData, int audioChannel) {
     	if (!isEnabled()) {
     		return;
     	}
@@ -244,7 +244,7 @@ public class ExternalDecoder {
     		return;
     	}
 
-    	decodeExtAudio(mpegData, mpegFileSize, mpegOffset);
+    	decodeExtAudio(mpegData, mpegFileSize, mpegOffset, audioChannel);
     }
 
     private static String getAtracAudioPath(int address, int atracFileSize, String suffix) {

src/jpcsp/media/MediaEngine.java
@@ -316,7 +316,7 @@ public class MediaEngine {
      * The sceMpeg functions must call init() first for each MPEG stream and then
      * keep calling step() until the video is finished and finish() is called.
      */
-    public void init(IURLProtocolHandler channel, boolean decodeVideo, boolean decodeAudio) {
+    public void init(IURLProtocolHandler channel, boolean decodeVideo, boolean decodeAudio, int audioChannel) {
     	init();
 
     	container = IContainer.make();
@@ -374,7 +374,7 @@ public class MediaEngine {
         if (decodeAudio) {
             if (audioStreamID == -1) {
             	// Try to use an external audio file instead
-            	if (!initExtAudio()) {
+            	if (!initExtAudio(audioChannel)) {
             		log.error("MediaEngine: No audio streams found!");
             		audioStreamState = new StreamState(this, -1, null, sceMpeg.audioFirstTimestamp);
             	}
@@ -541,6 +541,13 @@ public class MediaEngine {
         return String.format("%sExtAudio.%s", getExtAudioBasePath(mpegStreamSize), suffix);
     }
 
+    public static String getExtAudioPath(int mpegStreamSize, int audioChannel, String suffix) {
+    	if (audioChannel < 0) {
+    		return getExtAudioPath(mpegStreamSize, suffix);
+    	}
+        return String.format("%sExtAudio-%d.%s", getExtAudioBasePath(mpegStreamSize), audioChannel, suffix);
+    }
+
     public boolean stepVideo() {
     	return step(videoStreamState, 0);
     }
@@ -570,10 +577,10 @@ public class MediaEngine {
         return complete;
     }
 
-    private File getExtAudioFile() {
+    private File getExtAudioFile(int audioChannel) {
         String supportedFormats[] = {"wav", "mp3", "at3", "raw", "wma", "flac", "m4a"};
         for (int i = 0; i < supportedFormats.length; i++) {
-            File f = new File(getExtAudioPath(bufferSize, supportedFormats[i]));
+            File f = new File(getExtAudioPath(bufferSize, audioChannel, supportedFormats[i]));
             if (f.canRead() && f.length() > 0) {
             	return f;
             }
@@ -582,20 +589,20 @@ public class MediaEngine {
         return null;
     }
 
-    private boolean initExtAudio() {
+    private boolean initExtAudio(int audioChannel) {
     	boolean useExtAudio = false;
 
-    	File extAudioFile = getExtAudioFile();
+    	File extAudioFile = getExtAudioFile(audioChannel);
     	if (extAudioFile == null && ExternalDecoder.isEnabled()) {
     		// Try to decode the audio using the external decoder
     		if (bufferAddress == 0) {
     			if (bufferData != null) {
-    				externalDecoder.decodeExtAudio(bufferData, bufferSize, bufferMpegOffset);
+    				externalDecoder.decodeExtAudio(bufferData, bufferSize, bufferMpegOffset, audioChannel);
     			}
     		} else {
-    			externalDecoder.decodeExtAudio(bufferAddress, bufferSize, bufferMpegOffset, bufferData);
+    			externalDecoder.decodeExtAudio(bufferAddress, bufferSize, bufferMpegOffset, bufferData, audioChannel);
     		}
-			extAudioFile = getExtAudioFile();
+			extAudioFile = getExtAudioFile(audioChannel);
     	}
 
     	if (extAudioFile != null) {

commit 09f5ee6b8304d8df2e4d7245e7c2a462c577d3e4

Video Mpeg video demux broken by r2796.

src/jpcsp/media/MpegDemux.java
@@ -250,9 +250,11 @@ public class MpegDemux {
 				case 0x1E8: case 0x1E9: case 0x1EA: case 0x1EB:
 				case 0x1EC: case 0x1ED: case 0x1EE: case 0x1EF: {
 					// Video Stream
-					if ((startCode & 0x0F) == videoChannel) {
-						demuxStream(demuxVideo, startCode, videoChannel, videoStream, true);
+					boolean demuxStream = demuxVideo;
+					if (videoChannel >= 0 && (startCode & 0x0F) != videoChannel) {
+						demuxStream = false;
 					}
+					demuxStream(demuxStream, startCode, videoChannel, videoStream, true);
 					break;
 				}
 				default: {