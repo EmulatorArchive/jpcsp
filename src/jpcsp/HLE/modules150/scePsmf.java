EmulatorArchive/jpcsp

commit 0dc24b6ea70e1b43ac5967209600011b90bc5673

Improved error handling in scePsmfSpecifyStreamWithStreamType, scePsmfGetVideoInfo and scePsmfGetAudioInfo to better match the PSP.
git-svn-id: http://jpcsp.googlecode.com/svn/trunk@3153 4ab978e6-3352-0410-996c-0b42db0051c8

src/jpcsp/HLE/modules150/scePsmf.java
@@ -375,6 +375,10 @@ public class scePsmf extends HLEModule {
             return currentStreamNumber;
         }
 
+        public boolean isValidCurrentStreamNumber() {
+        	return getCurrentStreamNumber() >= 0;
+        }
+
         public int getCurrentStreamType() {
             if (currentStreamNumber >= 0 && currentStreamNumber < streams.size()) {
                 return streams.get(currentStreamNumber).getStreamType();
@@ -417,16 +421,18 @@ public class scePsmf extends HLEModule {
         public void setStreamNum(int id) {
             currentStreamNumber = id;
 
-            int type = getCurrentStreamType();
-            int channel = getCurrentStreamChannel();
-            switch (type) {
-            	case PSMF_AVC_STREAM:
-            		setVideoStreamNum(id, channel);
-            		break;
-            	case PSMF_PCM_STREAM:
-            	case PSMF_ATRAC_STREAM:
-            		setAudioStreamNum(id, channel);
-            		break;
+            if (isValidCurrentStreamNumber()) {
+	            int type = getCurrentStreamType();
+	            int channel = getCurrentStreamChannel();
+	            switch (type) {
+	            	case PSMF_AVC_STREAM:
+	            		setVideoStreamNum(id, channel);
+	            		break;
+	            	case PSMF_PCM_STREAM:
+	            	case PSMF_ATRAC_STREAM:
+	            		setAudioStreamNum(id, channel);
+	            		break;
+	            }
             }
         }
 
@@ -558,7 +564,8 @@ public class scePsmf extends HLEModule {
     public int scePsmfSpecifyStreamWithStreamType(@CheckArgument("checkPsmf") TPointer32 psmf, int type, int ch) {
         PSMFHeader header = getPsmfHeader(psmf);
         if (!header.setStreamWithType(type, ch)) {
-        	return SceKernelErrors.ERROR_PSMF_INVALID_ID;
+        	// Do not return SceKernelErrors.ERROR_PSMF_INVALID_ID, but set an invalid stream number.
+        	header.setStreamNum(-1);
         }
 
         return 0;
@@ -613,6 +620,9 @@ public class scePsmf extends HLEModule {
     @HLEFunction(nid = 0x0BA514E5, version = 150, checkInsideInterrupt = true)
     public int scePsmfGetVideoInfo(@CheckArgument("checkPsmf") TPointer32 psmf, TPointer32 videoInfoAddr) {
         PSMFHeader header = getPsmfHeader(psmf);
+        if (!header.isValidCurrentStreamNumber()) {
+        	return SceKernelErrors.ERROR_PSMF_INVALID_ID;
+        }
         videoInfoAddr.setValue(0, header.getVideoWidth());
         videoInfoAddr.setValue(4, header.getvideoHeigth());
 
@@ -622,6 +632,9 @@ public class scePsmf extends HLEModule {
     @HLEFunction(nid = 0xA83F7113, version = 150, checkInsideInterrupt = true)
     public int scePsmfGetAudioInfo(@CheckArgument("checkPsmf") TPointer32 psmf, TPointer32 audioInfoAddr) {
         PSMFHeader header = getPsmfHeader(psmf);
+        if (!header.isValidCurrentStreamNumber()) {
+        	return SceKernelErrors.ERROR_PSMF_INVALID_ID;
+        }
         audioInfoAddr.setValue(0, header.getAudioChannelConfig());
         audioInfoAddr.setValue(4, header.getAudioSampleFrequency());
 

commit 06d19e88f6976a3e84b36b32bf0624e7d8af893a

Fixed scePsmfGetVideoInfo (issue introduced by r3153): the PSP seems to default to the stream #0 as current stream.

src/jpcsp/HLE/modules150/scePsmf.java
@@ -280,6 +280,11 @@ public class scePsmf extends HLEModule {
 
             streams = readPsmfStreams(mem, addr, null, this);
 
+            // PSP seems to default to stream 0.
+            if (streams.size() > 0) {
+            	setStreamNum(0);
+            }
+
             // EPMap info:
             // - Located at EPMapOffset (set by the AVC stream);
             // - Each entry is composed by a total of 10 bytes:
@@ -485,7 +490,7 @@ public class scePsmf extends HLEModule {
 
     	// Stream area:
         // At offset 0x82, each 16 bytes represent one stream.
-        LinkedList<PSMFStream>streams = new LinkedList<PSMFStream>();
+        LinkedList<PSMFStream> streams = new LinkedList<PSMFStream>();
 
         // Parse the stream field and assign each one to it's type.
         int numberOfStreams = 0;