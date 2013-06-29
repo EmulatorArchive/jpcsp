EmulatorArchive/jpcsp

commit 0b32ef506dcb2aa57a4a77a4cabb1f76d7e1b7f3

updated i18n for GUI elements plus small fixes
MainGUI:
* added loading of default locale or user specified locale
* added menu item for language setting 'system locale'
* ressource string names and variable names reworked for proper i18n
* language names coded in GUI form as they won't be changed
* initial location of log GUI set to parent center
* locale code of catalan corrected to ca_ES
* code cleanup

common.properties:
- removed as no longer needed by MainGUI

DefaultSettings.properties:
* default language set to 'system locale'
* added emu.useDebuggerMemory

languages directory:
* converted language files for new type localisation
* reused most of the existing translations
* completely reworked German translation

MemoryViewer:
* form and contents made resizeable
* resource string names and variable names reworked for proper i18n
* code cleanup

InstructionCounter:
* rows make sortable
* number of rows adjusted to found opcodes
* code an UI cleanup

RegisterTable:
* adapted to be integrateble in NetBeans UI designer
* code cleanup

DisassemblerFrame:
* adapted for new RegisterTable
* ressource string names and variable names reworked for proper i18n
* OptionModePane replaced by proper UI form (DumpCodeDialog)
* file dialog filter definitions moved to Utilities
* dialog made resizeable

MemoryBreakpointsDialog:
* file dialog filter definitions moved to Utilities

CheatsGUI:
* redesigned for i18n using NetBeans

CancelButton:
* adapted to be integrateble in NetBeans UI designer

MemStickBrowser:
* code cleanup
* implemented list refreshing
* resource string names and variable names reworked for proper i18n

LogGUI:
* dialog will open by default centered in parent
* ressurce string names and variable names reworked for proper i18n

OptionPaneMultiple:
- removed, superceeded by DumpCodeDialog

Utilities:
* moved charset to Constants
* code formatting

LogWindow:
* resource string names and variable names reworked for proper i18n

ElfHeaderInfo:

src/jpcsp/languages/common.properties
@@ -1,20 +1,0 @@
-# Language independent resources
-english=English
-french=Fran\u00E7ais
-german=Deutsch
-lithuanian=Lietuvi\u0173
-spanish=Espa\u00F1ol
-catalan=Catal\u00E0
-portuguesebr=Portugu\u00EAs Brasileiro
-portuguese=Portugu\u00EAs
-japanese=\u65E5\u672C\u8A9E
-russian=\u0420\u0443\u0441\u0441\u043A\u0438\u0439
-polish=Polski
-chinesePRC=\u7B80\u4F53\u4E2D\u6587
-chineseTW=\u6B63\u9AD4\u4E2D\u6587
-italian=Italiano
-dutch=Nederlands
-korean=Korean
-traditionalChinese=Traditional Chinese
-simplifiedChinese=Simplified Chinese
-greek=\u0395\u03BB\u03BB\u03B7\u03BD\u03B9\u03BA\u03AC

commit 1c328db438814fed0755e302d2e55bd6bf1ee46f

fixed regression from r3265 - language strings in main menu are not displayed properly on all systems

src/jpcsp/MainGUI.form
@@ -727,7 +727,9 @@
                 <Property name="icon" type="javax.swing.Icon" editor="org.netbeans.modules.form.editors2.IconEditor">
                   <Image iconType="3" name="/jpcsp/icons/flags/en_EN.png"/>
                 </Property>
-                <Property name="text" type="java.lang.String" value="English" noResource="true"/>
+                <Property name="text" type="java.lang.String" editor="org.netbeans.modules.i18n.form.FormI18nStringEditor">
+                  <ResourceString bundle="jpcsp/languages/common.properties" key="english" replaceFormat="java.util.ResourceBundle.getBundle(&quot;{bundleNameSlashes}&quot;).getString(&quot;{key}&quot;)"/>
+                </Property>
               </Properties>
               <Events>
                 <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="EnglishActionPerformed"/>
@@ -738,7 +740,9 @@
                 <Property name="icon" type="javax.swing.Icon" editor="org.netbeans.modules.form.editors2.IconEditor">
                   <Image iconType="3" name="/jpcsp/icons/flags/fr_FR.png"/>
                 </Property>
-                <Property name="text" type="java.lang.String" value="Fran&#xe7;ais" noResource="true"/>
+                <Property name="text" type="java.lang.String" editor="org.netbeans.modules.i18n.form.FormI18nStringEditor">
+                  <ResourceString bundle="jpcsp/languages/common.properties" key="french" replaceFormat="java.util.ResourceBundle.getBundle(&quot;{bundleNameSlashes}&quot;).getString(&quot;{key}&quot;)"/>
+                </Property>
               </Properties>
               <Events>
                 <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="FrenchActionPerformed"/>
@@ -749,7 +753,9 @@
                 <Property name="icon" type="javax.swing.Icon" editor="org.netbeans.modules.form.editors2.IconEditor">
                   <Image iconType="3" name="/jpcsp/icons/flags/de_DE.png"/>
                 </Property>
-                <Property name="text" type="java.lang.String" value="Deutsch" noResource="true"/>
+                <Property name="text" type="java.lang.String" editor="org.netbeans.modules.i18n.form.FormI18nStringEditor">
+                  <ResourceString bundle="jpcsp/languages/common.properties" key="german" replaceFormat="java.util.ResourceBundle.getBundle(&quot;{bundleNameSlashes}&quot;).getString(&quot;{key}&quot;)"/>
+                </Property>
               </Properties>
               <Events>
                 <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="GermanActionPerformed"/>
@@ -760,7 +766,9 @@
                 <Property name="icon" type="javax.swing.Icon" editor="org.netbeans.modules.form.editors2.IconEditor">
                   <Image iconType="3" name="/jpcsp/icons/flags/lt_LT.png"/>
                 </Property>
-                <Property name="text" type="java.lang.String" value="Lietuvi&#x173;" noResource="true"/>
+                <Property name="text" type="java.lang.String" editor="org.netbeans.modules.i18n.form.FormI18nStringEditor">
+                  <ResourceString bundle="jpcsp/languages/common.properties" key="lithuanian" replaceFormat="java.util.ResourceBundle.getBundle(&quot;{bundleNameSlashes}&quot;).getString(&quot;{key}&quot;)"/>
+                </Property>
               </Properties>
               <Events>
                 <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="LithuanianActionPerformed"/>
@@ -771,7 +779,9 @@
                 <Property name="icon" type="javax.swing.Icon" editor="org.netbeans.modules.form.editors2.IconEditor">
                   <Image iconType="3" name="/jpcsp/icons/flags/es_ES.png"/>
                 </Property>
-                <Property name="text" type="java.lang.String" value="Espa&#xf1;ol" noResource="true"/>
+                <Property name="text" type="java.lang.String" editor="org.netbeans.modules.i18n.form.FormI18nStringEditor">
+                  <ResourceString bundle="jpcsp/languages/common.properties" key="spanish" replaceFormat="java.util.ResourceBundle.getBundle(&quot;{bundleNameSlashes}&quot;).getString(&quot;{key}&quot;)"/>
+                </Property>
               </Properties>
               <Events>
                 <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="SpanishActionPerformed"/>
@@ -782,7 +792,9 @@
                 <Property name="icon" type="javax.swing.Icon" editor="org.netbeans.modules.form.editors2.IconEditor">
                   <Image iconType="3" name="/jpcsp/icons/flags/es_CA.png"/>
                 </Property>
-                <Property name="text" type="java.lang.String" value="Catal&#xe0;" noResource="true"/>
+                <Property name="text" type="java.lang.String" editor="org.netbeans.modules.i18n.form.FormI18nStringEditor">
+                  <ResourceString bundle="jpcsp/languages/common.properties" key="catalan" replaceFormat="java.util.ResourceBundle.getBundle(&quot;{bundleNameSlashes}&quot;).getString(&quot;{key}&quot;)"/>
+                </Property>
               </Properties>
               <Events>
                 <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="CatalanActionPerformed"/>
@@ -793,7 +805,9 @@
                 <Property name="icon" type="javax.swing.Icon" editor="org.netbeans.modules.form.editors2.IconEditor">
                   <Image iconType="3" name="/jpcsp/icons/flags/pt_PT.png"/>
                 </Property>
-                <Property name="text" type="java.lang.String" value="Portugu&#xea;s" noResource="true"/>
+                <Property name="text" type="java.lang.String" editor="org.netbeans.modules.i18n.form.FormI18nStringEditor">
+                  <ResourceString bundle="jpcsp/languages/common.properties" key="portuguese" replaceFormat="java.util.ResourceBundle.getBundle(&quot;{bundleNameSlashes}&quot;).getString(&quot;{key}&quot;)"/>
+                </Property>
               </Properties>
               <Events>
                 <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="PortugueseActionPerformed"/>
@@ -804,7 +818,9 @@
                 <Property name="icon" type="javax.swing.Icon" editor="org.netbeans.modules.form.editors2.IconEditor">
                   <Image iconType="3" name="/jpcsp/icons/flags/pt_BR.png"/>
                 </Property>
-                <Property name="text" type="java.lang.String" value="Portugu&#xea;s Brasileiro" noResource="true"/>
+                <Property name="text" type="java.lang.String" editor="org.netbeans.modules.i18n.form.FormI18nStringEditor">
+                  <ResourceString bundle="jpcsp/languages/common.properties" key="portuguesebr" replaceFormat="java.util.ResourceBundle.getBundle(&quot;{bundleNameSlashes}&quot;).getString(&quot;{key}&quot;)"/>
+                </Property>
               </Properties>
               <Events>
                 <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="PortugueseBRActionPerformed"/>
@@ -815,7 +831,9 @@
                 <Property name="icon" type="javax.swing.Icon" editor="org.netbeans.modules.form.editors2.IconEditor">
                   <Image iconType="3" name="/jpcsp/icons/flags/jp_JP.png"/>
                 </Property>
-                <Property name="text" type="java.lang.String" value="&#x65e5;&#x672c;&#x8a9e;" noResource="true"/>
+                <Property name="text" type="java.lang.String" editor="org.netbeans.modules.i18n.form.FormI18nStringEditor">
+                  <ResourceString bundle="jpcsp/languages/common.properties" key="japanese" replaceFormat="java.util.ResourceBundle.getBundle(&quot;{bundleNameSlashes}&quot;).getString(&quot;{key}&quot;)"/>
+                </Property>
               </Properties>
               <Events>
                 <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="JapaneseActionPerformed"/>
@@ -826,7 +844,9 @@
                 <Property name="icon" type="javax.swing.Icon" editor="org.netbeans.modules.form.editors2.IconEditor">
                   <Image iconType="3" name="/jpcsp/icons/flags/ru_RU.png"/>
                 </Property>
-                <Property name="text" type="java.lang.String" value="&#x420;&#x443;&#x441;&#x441;&#x43a;&#x438;&#x439;" noResource="true"/>
+                <Property name="text" type="java.lang.String" editor="org.netbeans.modules.i18n.form.FormI18nStringEditor">
+                  <ResourceString bundle="jpcsp/languages/common.properties" key="russian" replaceFormat="java.util.ResourceBundle.getBundle(&quot;{bundleNameSlashes}&quot;).getString(&quot;{key}&quot;)"/>
+                </Property>
               </Properties>
               <Events>
                 <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="RussianActionPerformed"/>
@@ -837,7 +857,9 @@
                 <Property name="icon" type="javax.swing.Icon" editor="org.netbeans.modules.form.editors2.IconEditor">
                   <Image iconType="3" name="/jpcsp/icons/flags/pl_PL.png"/>
                 </Property>
-                <Property name="text" type="java.lang.String" value="Polski" noResource="true"/>
+                <Property name="text" type="java.lang.String" editor="org.netbeans.modules.i18n.form.FormI18nStringEditor">
+                  <ResourceString bundle="jpcsp/languages/common.properties" key="polish" replaceFormat="java.util.ResourceBundle.getBundle(&quot;{bundleNameSlashes}&quot;).getString(&quot;{key}&quot;)"/>
+                </Property>
               </Properties>
               <Events>
                 <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="PolishActionPerformed"/>
@@ -848,7 +870,9 @@
                 <Property name="icon" type="javax.swing.Icon" editor="org.netbeans.modules.form.editors2.IconEditor">
                   <Image iconType="3" name="/jpcsp/icons/flags/cn_CN.png"/>
                 </Property>
-                <Property name="text" type="java.lang.String" value="&#x7b80;&#x4f53;&#x4e2d;&#x6587;" noResource="true"/>
+                <Property name="text" type="java.lang.String" editor="org.netbeans.modules.i18n.form.FormI18nStringEditor">
+                  <ResourceString bundle="jpcsp/languages/common.properties" key="simplifiedChinese" replaceFormat="java.util.ResourceBundle.getBundle(&quot;{bundleNameSlashes}&quot;).getString(&quot;{key}&quot;)"/>
+                </Property>
               </Properties>
               <Events>
                 <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="ChinesePRCActionPerformed"/>
@@ -859,7 +883,9 @@
                 <Property name="icon" type="javax.swing.Icon" editor="org.netbeans.modules.form.editors2.IconEditor">
                   <Image iconType="3" name="/jpcsp/icons/flags/tw_TW.png"/>
                 </Property>
-                <Property name="text" type="java.lang.String" value="&#x6b63;&#x9ad4;&#x4e2d;&#x6587;" noResource="true"/>
+                <Property name="text" type="java.lang.String" editor="org.netbeans.modules.i18n.form.FormI18nStringEditor">
+                  <ResourceString bundle="jpcsp/languages/common.properties" key="traditionalChinese" replaceFormat="java.util.ResourceBundle.getBundle(&quot;{bundleNameSlashes}&quot;).getString(&quot;{key}&quot;)"/>
+                </Property>
               </Properties>
               <Events>
                 <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="ChineseTWActionPerformed"/>
@@ -870,7 +896,9 @@
                 <Property name="icon" type="javax.swing.Icon" editor="org.netbeans.modules.form.editors2.IconEditor">
                   <Image iconType="3" name="/jpcsp/icons/flags/it_IT.png"/>
                 </Property>
-                <Property name="text" type="java.lang.String" value="Italiano" noResource="true"/>
+                <Property name="text" type="java.lang.String" editor="org.netbeans.modules.i18n.form.FormI18nStringEditor">
+                  <ResourceString bundle="jpcsp/languages/common.properties" key="italian" replaceFormat="java.util.ResourceBundle.getBundle(&quot;{bundleNameSlashes}&quot;).getString(&quot;{key}&quot;)"/>
+                </Property>
               </Properties>
               <Events>
                 <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="ItalianActionPerformed"/>
@@ -881,7 +909,9 @@
                 <Property name="icon" type="javax.swing.Icon" editor="org.netbeans.modules.form.editors2.IconEditor">
                   <Image iconType="3" name="/jpcsp/icons/flags/gr_EL.png"/>
                 </Property>
-                <Property name="text" type="java.lang.String" value="&#x395;&#x3bb;&#x3bb;&#x3b7;&#x3bd;&#x3b9;&#x3ba;&#x3ac;" noResource="true"/>
+                <Property name="text" type="java.lang.String" editor="org.netbeans.modules.i18n.form.FormI18nStringEditor">
+                  <ResourceString bundle="jpcsp/languages/common.properties" key="greek" replaceFormat="java.util.ResourceBundle.getBundle(&quot;{bundleNameSlashes}&quot;).getString(&quot;{key}&quot;)"/>
+                </Property>
               </Properties>
               <Events>
                 <EventHandler event="actionPerformed" listener="java.awt.event.ActionListener" parameters="java.awt.event.ActionEvent" handler="GreekActionPerformed"/>

src/jpcsp/MainGUI.java
@@ -871,7 +871,8 @@ public class MainGUI extends javax.swing.JFrame implements KeyListener, Componen
         LanguageMenu.add(SystemLocale);
 
         English.setIcon(new javax.swing.ImageIcon(getClass().getResource("/jpcsp/icons/flags/en_EN.png"))); // NOI18N
-        English.setText("English"); // NOI18N
+        java.util.ResourceBundle bundle1 = java.util.ResourceBundle.getBundle("jpcsp/languages/common"); // NOI18N
+        English.setText(bundle1.getString("english")); // NOI18N
         English.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 EnglishActionPerformed(evt);
@@ -880,7 +881,7 @@ public class MainGUI extends javax.swing.JFrame implements KeyListener, Componen
         LanguageMenu.add(English);
 
         French.setIcon(new javax.swing.ImageIcon(getClass().getResource("/jpcsp/icons/flags/fr_FR.png"))); // NOI18N
-        French.setText("Français"); // NOI18N
+        French.setText(bundle1.getString("french")); // NOI18N
         French.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 FrenchActionPerformed(evt);
@@ -889,7 +890,7 @@ public class MainGUI extends javax.swing.JFrame implements KeyListener, Componen
         LanguageMenu.add(French);
 
         German.setIcon(new javax.swing.ImageIcon(getClass().getResource("/jpcsp/icons/flags/de_DE.png"))); // NOI18N
-        German.setText("Deutsch"); // NOI18N
+        German.setText(bundle1.getString("german")); // NOI18N
         German.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 GermanActionPerformed(evt);
@@ -898,7 +899,7 @@ public class MainGUI extends javax.swing.JFrame implements KeyListener, Componen
         LanguageMenu.add(German);
 
         Lithuanian.setIcon(new javax.swing.ImageIcon(getClass().getResource("/jpcsp/icons/flags/lt_LT.png"))); // NOI18N
-        Lithuanian.setText("Lietuvių"); // NOI18N
+        Lithuanian.setText(bundle1.getString("lithuanian")); // NOI18N
         Lithuanian.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 LithuanianActionPerformed(evt);
@@ -907,7 +908,7 @@ public class MainGUI extends javax.swing.JFrame implements KeyListener, Componen
         LanguageMenu.add(Lithuanian);
 
         Spanish.setIcon(new javax.swing.ImageIcon(getClass().getResource("/jpcsp/icons/flags/es_ES.png"))); // NOI18N
-        Spanish.setText("Español"); // NOI18N
+        Spanish.setText(bundle1.getString("spanish")); // NOI18N
         Spanish.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 SpanishActionPerformed(evt);
@@ -916,7 +917,7 @@ public class MainGUI extends javax.swing.JFrame implements KeyListener, Componen
         LanguageMenu.add(Spanish);
 
         Catalan.setIcon(new javax.swing.ImageIcon(getClass().getResource("/jpcsp/icons/flags/es_CA.png"))); // NOI18N
-        Catalan.setText("Català"); // NOI18N
+        Catalan.setText(bundle1.getString("catalan")); // NOI18N
         Catalan.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 CatalanActionPerformed(evt);
@@ -925,7 +926,7 @@ public class MainGUI extends javax.swing.JFrame implements KeyListener, Componen
         LanguageMenu.add(Catalan);
 
         Portuguese.setIcon(new javax.swing.ImageIcon(getClass().getResource("/jpcsp/icons/flags/pt_PT.png"))); // NOI18N
-        Portuguese.setText("Português"); // NOI18N
+        Portuguese.setText(bundle1.getString("portuguese")); // NOI18N
         Portuguese.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 PortugueseActionPerformed(evt);
@@ -934,7 +935,7 @@ public class MainGUI extends javax.swing.JFrame implements KeyListener, Componen
         LanguageMenu.add(Portuguese);
 
         PortugueseBR.setIcon(new javax.swing.ImageIcon(getClass().getResource("/jpcsp/icons/flags/pt_BR.png"))); // NOI18N
-        PortugueseBR.setText("Português Brasileiro"); // NOI18N
+        PortugueseBR.setText(bundle1.getString("portuguesebr")); // NOI18N
         PortugueseBR.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 PortugueseBRActionPerformed(evt);
@@ -943,7 +944,7 @@ public class MainGUI extends javax.swing.JFrame implements KeyListener, Componen
         LanguageMenu.add(PortugueseBR);
 
         Japanese.setIcon(new javax.swing.ImageIcon(getClass().getResource("/jpcsp/icons/flags/jp_JP.png"))); // NOI18N
-        Japanese.setText("日本語"); // NOI18N
+        Japanese.setText(bundle1.getString("japanese")); // NOI18N
         Japanese.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 JapaneseActionPerformed(evt);
@@ -952,7 +953,7 @@ public class MainGUI extends javax.swing.JFrame implements KeyListener, Componen
         LanguageMenu.add(Japanese);
 
         Russian.setIcon(new javax.swing.ImageIcon(getClass().getResource("/jpcsp/icons/flags/ru_RU.png"))); // NOI18N
-        Russian.setText("Русский"); // NOI18N
+        Russian.setText(bundle1.getString("russian")); // NOI18N
         Russian.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 RussianActionPerformed(evt);
@@ -961,7 +962,7 @@ public class MainGUI extends javax.swing.JFrame implements KeyListener, Componen
         LanguageMenu.add(Russian);
 
         Polish.setIcon(new javax.swing.ImageIcon(getClass().getResource("/jpcsp/icons/flags/pl_PL.png"))); // NOI18N
-        Polish.setText("Polski"); // NOI18N
+        Polish.setText(bundle1.getString("polish")); // NOI18N
         Polish.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 PolishActionPerformed(evt);
@@ -970,7 +971,7 @@ public class MainGUI extends javax.swing.JFrame implements KeyListener, Componen
         LanguageMenu.add(Polish);
 
         ChinesePRC.setIcon(new javax.swing.ImageIcon(getClass().getResource("/jpcsp/icons/flags/cn_CN.png"))); // NOI18N
-        ChinesePRC.setText("简体中文"); // NOI18N
+        ChinesePRC.setText(bundle1.getString("simplifiedChinese")); // NOI18N
         ChinesePRC.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 ChinesePRCActionPerformed(evt);
@@ -979,7 +980,7 @@ public class MainGUI extends javax.swing.JFrame implements KeyListener, Componen
         LanguageMenu.add(ChinesePRC);
 
         ChineseTW.setIcon(new javax.swing.ImageIcon(getClass().getResource("/jpcsp/icons/flags/tw_TW.png"))); // NOI18N
-        ChineseTW.setText("正體中文"); // NOI18N
+        ChineseTW.setText(bundle1.getString("traditionalChinese")); // NOI18N
         ChineseTW.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 ChineseTWActionPerformed(evt);
@@ -988,7 +989,7 @@ public class MainGUI extends javax.swing.JFrame implements KeyListener, Componen
         LanguageMenu.add(ChineseTW);
 
         Italian.setIcon(new javax.swing.ImageIcon(getClass().getResource("/jpcsp/icons/flags/it_IT.png"))); // NOI18N
-        Italian.setText("Italiano"); // NOI18N
+        Italian.setText(bundle1.getString("italian")); // NOI18N
         Italian.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 ItalianActionPerformed(evt);
@@ -997,7 +998,7 @@ public class MainGUI extends javax.swing.JFrame implements KeyListener, Componen
         LanguageMenu.add(Italian);
 
         Greek.setIcon(new javax.swing.ImageIcon(getClass().getResource("/jpcsp/icons/flags/gr_EL.png"))); // NOI18N
-        Greek.setText("Ελληνικά"); // NOI18N
+        Greek.setText(bundle1.getString("greek")); // NOI18N
         Greek.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 GreekActionPerformed(evt);

src/jpcsp/languages/common.properties
@@ -1,0 +1,19 @@
+english=English
+french=Fran\u00e7ais
+german=Deutsch
+lithuanian=Lietuvi\u0173
+spanish=Espa\u00f1ol
+catalan=Catal\u00e0
+portuguesebr=Portugu\u00eas Brasileiro
+portuguese=Portugu\u00eas
+japanese=\u65e5\u672c\u8a9e
+russian=\u0420\u0443\u0441\u0441\u043a\u0438\u0439
+polish=Polski
+chinesePRC=\u7b80\u4f53\u4e2d\u6587
+chineseTW=\u6b63\u9ad4\u4e2d\u6587
+italian=Italiano
+dutch=Nederlands
+korean=Korean
+traditionalChinese=\u6b63\u9ad4\u4e2d\u6587
+simplifiedChinese=\u7b80\u4f53\u4e2d\u6587
+greek=\u0395\u03bb\u03bb\u03b7\u03bd\u03b9\u03ba\u03ac