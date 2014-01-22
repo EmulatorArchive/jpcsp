EmulatorArchive/jpcsp

commit c278e2857268bde63ad48e080bdbcfedd904d5dc

fixed warning on Sun proprietary API usage minor code cleanup
git-svn-id: http://jpcsp.googlecode.com/svn/trunk@3444 4ab978e6-3352-0410-996c-0b42db0051c8

src/jpcsp/ParameterReader.java
@@ -20,13 +20,11 @@ import static jpcsp.Allegrex.Common._a0;
 import static jpcsp.Allegrex.Common._f0;
 import static jpcsp.Allegrex.Common._f12;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import jpcsp.Allegrex.CpuState;
 
 public class ParameterReader {
 	private CpuState cpu;
-	private Memory memory;
+	final private Memory memory;
 	private int parameterIndex = 0;
 	private int parameterIndexFloat = 0;
 	protected static final int maxParameterInGprRegisters = 8;
@@ -57,7 +55,7 @@ public class ParameterReader {
 
 	private float getParameterFloatAt(int index) {
 		if (index >= maxParameterInFprRegisters) {
-			throw(new NotImplementedException());
+			throw(new UnsupportedOperationException());
 		}
 		return cpu.fpr[firstParameterInFpr + index];
 	}
@@ -109,7 +107,7 @@ public class ParameterReader {
 	}
 
 	public void setReturnValueLong(long value) {
-		cpu._v0 = (int)((value >>  0) & 0xFFFFFFFF);
+		cpu._v0 = (int)(                0xFFFFFFFF);
 		cpu._v1 = (int)((value >> 32) & 0xFFFFFFFF);
 	}
 }

src/jpcsp/Allegrex/compiler/CompilerParameterReader.java
@@ -21,8 +21,6 @@ import static jpcsp.Allegrex.Common._sp;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import jpcsp.ParameterReader;
 
 /**
@@ -30,7 +28,7 @@ import jpcsp.ParameterReader;
  *
  */
 public class CompilerParameterReader extends ParameterReader {
-	private ICompilerContext compilerContext;
+	final private ICompilerContext compilerContext;
 	private boolean hasErrorPointer = false;
 	private int currentParameterIndex = 0;
 	private int currentStackPopIndex = 0;
@@ -51,7 +49,7 @@ public class CompilerParameterReader extends ParameterReader {
 
 	private void loadParameterFloatAt(int index) {
 		if (index >= maxParameterInFprRegisters) {
-			throw(new NotImplementedException());
+			throw(new UnsupportedOperationException());
 		}
 		compilerContext.loadFRegister(firstParameterInFpr + index);
 	}

commit 13a8e275e335806a91f1ec04e00d839b12923aa9

fixed regression from r3444

src/jpcsp/ParameterReader.java
@@ -107,7 +107,7 @@ public class ParameterReader {
 	}
 
 	public void setReturnValueLong(long value) {
-		cpu._v0 = (int)(                0xFFFFFFFF);
+		cpu._v0 = (int)((value      ) & 0xFFFFFFFF);
 		cpu._v1 = (int)((value >> 32) & 0xFFFFFFFF);
 	}
 }